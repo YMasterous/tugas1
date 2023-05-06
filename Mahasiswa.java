3d83aa78b96cb252d6dad84dc411254ab0a1604f
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-05-06 15:13:37 +0700

    Tambah file Mahasiswa dan tugas-algo1

---
+++ Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+    private String npm;
+    private String nama;
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+}
